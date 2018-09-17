d7bb15a2758648664336354ba196f0c8b34e72c7
BRIAN <bdicola@gmail.com>
2018-09-17 16:26:24 -0400

    Create and set up new bucket

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -2,6 +2,7 @@ import boto3
 import click
 
 session = boto3.Session(profile_name='pythonAutomation')
+print("session.region_name is: %s" % session.region_name)
 s3 = session.resource('s3')
 
 @click.group()
@@ -21,6 +22,42 @@ def list_bucket_objects(bucket):
     "List objects in an s3 bucket"
     for obj in s3.Bucket(bucket).objects.all():
         print (obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure S3 bucket"
+    s3_bucket = s3.create_bucket(
+        Bucket=bucket
+        )
+
+    policy = """
+        {
+        "Version":"2012-10-17",
+        "Statement":[{
+        "Sid":"PublicReadGetObject",
+        "Effect":"Allow",
+        "Principal": "*",
+            "Action":["s3:GetObject"],
+            "Resource":["arn:aws:s3:::%s/*"]
+        }]}
+        """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+        'ErrorDocument': {
+        'Key': 'error.html'
+        },
+        'IndexDocument': {
+        'Suffix': 'index.html'
+        }
+    })
+
+    return
 
 if __name__ == '__main__':
         cli()
